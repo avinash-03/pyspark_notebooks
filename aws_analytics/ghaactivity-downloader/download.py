2aa619cc07d7a0496159f22a6729a6e3709abe8d
avinash-03 <avinashgodbole100@gmail.com>
2023-06-15 20:46:12 +0530

    "updated ETL"

--- aws_analytics/ghaactivity-downloader/download.py
+++ aws_analytics/ghaactivity-downloader/download.py
@@ -2,10 +2,13 @@ import requests
 
 file = "2015-01-01-15.json.gz"
 
+
 def download_file(file):
     url = f"https://data.gharchive.org/{file}"
     res = requests.get(url)
     return res
 
-# res = download_file(file)
-# print(res.content)
+
+res = download_file(file)
+print(res.content)
+
